9a8130d2d540de148f1d6608631f9f56e4be7d1b
Josh Hartwig <joshman211@gmail.com>
2023-06-18 13:52:20 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './css/App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="app--title">
+        <h1>Quizzical</h1>
+        <p className="app--about">Quizzical is a quiz game that asks you a set of questions. You get scored based on the amount of correct answer you pick. Click Start Quiz to begin</p>
+        <button>Start Quiz</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Quiz.tsx
@@ -0,0 +1,14 @@
+/*
+  Our quick component will display question components. The question components
+  contain answers, with one or more answers being correct.
+*/
+
+import React from 'react'
+
+const Quiz = () => {
+  return (
+    <div>Quiz</div>
+  )
+}
+
+export default Quiz
